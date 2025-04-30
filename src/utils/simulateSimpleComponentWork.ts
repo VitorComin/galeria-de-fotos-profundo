ee1f2d145b862deec9edaec3a5e42ea37db74e6d
Vitor Comin <vitorgabrielcomin@gmail.com>
2025-04-29 22:38:05 -0300

    changing component complexity

--- src/utils/simulateSimpleComponentWork.ts
+++ src/utils/simulateSimpleComponentWork.ts
@@ -1,6 +1,6 @@
 export const simulateSimpleComponentWork = () => {
   let total = 0;
-  for (let i = 0; i < 10000; i++) {
+  for (let i = 0; i < 100000000; i++) {
     total += i;
   }
   return total;
