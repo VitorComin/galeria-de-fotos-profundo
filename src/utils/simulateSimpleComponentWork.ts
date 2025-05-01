30c5cd1f0add720b50294f327b31645adce2df2d
Vitor Comin <vitorgabrielcomin@gmail.com>
2025-05-01 16:04:47 -0300

    10_000_000 => 1e8

--- src/utils/simulateSimpleComponentWork.ts
+++ src/utils/simulateSimpleComponentWork.ts
@@ -1,6 +1,6 @@
 export const simulateSimpleComponentWork = () => {
   let total = 0;
-  for (let i = 0; i < 100000000; i++) {
+  for (let i = 0; i < 1e8; i++) {
     total += i;
   }
   return total;
